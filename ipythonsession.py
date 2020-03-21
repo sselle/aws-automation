c55ccabecce1269361f3568bdd284e9574f2034e
Sven Selle <s.selle@web.de>
2020-03-21 09:07:25 +0100

    explored first boto3 commands in ipython session

---
+++ ipythonsession.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import boto3
+session = boto3.Session()
+#ec2_client = session.client('ec2')
+s3 = session.resource('s3')
+
+# get all buckets
+for bucket in s3.buckets.all():
+    print(bucket)
+    
