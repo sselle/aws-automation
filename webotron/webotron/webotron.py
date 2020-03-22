01155ba1dabfdac7d6ac059ce2430a1b3b7fe71c
Sven Selle <s.selle@web.de>
2020-03-22 14:32:08 +0100

    cleaned up code in webotron script

--- webotron/webotron/webotron.py
+++ webotron/webotron/webotron.py
@@ -1,46 +1,61 @@
-import boto3
-import click
-from botocore.exceptions import ClientError
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""Webotron: Deploy websites with AWS.
+
+Webotron automates the process of deploying static web sites to AWS:
+- Configure AWS S3 buckets
+    - Create them
+    - Set them up for static website hosting
+    - Deploy local files to them
+- Configure DNS with AWS Route 53 - OPEN
+- Configure a CDN and SSL with AWS
+"""
+
 from pathlib import Path
 import mimetypes
 
-session = boto3.Session()
-s3 = session.resource('s3')
+import boto3
+from botocore.exceptions import ClientError
+import click
 
-# command structure
-# command, subcomand, option
+SESSION = boto3.Session()
+s3 = SESSION.resource('s3')
+
 
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
-    pass
+    """Webotron deploys websites to AWS."""
 
-@cli.command('list-buckets')      
+
+@cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"           # doc string - taucht automatisch in --help auf
+    """List all s3 buckets."""
     for bucket in s3.buckets.all():
         print(bucket)
+
 
 @cli.command('list-bucket-objects')
 @click.argument('bucket-name')
 def list_bucket_objects(bucket_name):
-    "List all objects from bucket"
+    """List all objects from bucket."""
     for obj in s3.Bucket(bucket_name).objects.all():
         print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket-name')
 def create_bucket(bucket_name):
-    "Create and configure "
+    """Create and configure."""
     s3_bucket = None
     try:
-        s3_bucket = s3.create_bucket(Bucket=bucket_name, 
-                                    CreateBucketConfiguration={'LocationConstraint': session.region_name})
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+        s3_bucket = s3.create_bucket(Bucket=bucket_name,
+                                     CreateBucketConfiguration={'LocationConstraint': SESSION.region_name})
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket_name)
         else:
-            raise e
+            raise error
 
     policy = policy = '''
     {ipy
@@ -55,15 +70,14 @@ def create_bucket(bucket_name):
             }
         ]
     }
-    '''% s3_bucket.name
+    ''' % s3_bucket.name
 
     policy = policy.strip()
 
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)
 
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={
+    s3_bucket.Website().put(WebsiteConfiguration={
         'ErrorDocument': {
             'Key': 'error.html'
         },
@@ -73,38 +87,42 @@ def create_bucket(bucket_name):
     })
 
     return
+
+
 def upload_file(s3_bucket, path, key):
+    """Upload files to s3 bucket."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
-    
+
     s3_bucket.upload_file(
         path,
         key,
         ExtraArgs={
-            'ContentType': 'text/html'
+            'ContentType': content_type
         })
+
 
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    "Sync local files from PATH to s3 BUCKET"
+    """Sync local files from PATH to s3 BUCKET."""
     s3_bucket = s3.Bucket(bucket)
 
-    root=Path(pathname).expanduser().resolve()
+    root = Path(pathname).expanduser().resolve()
+
     def handle_directory(target):
-        for p in target.iterdir():
-            if p.is_dir(): handle_directory(p)
-            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
-    
+        for path in target.iterdir():
+            if path.is_dir():
+                handle_directory(path)
+            if path.is_file():
+                upload_file(s3_bucket,
+                            str(path),
+                            str(path.relative_to(root)))
     handle_directory(root)
+
 
 def main():
     cli()
-    
+
+
 main()
-
-# create bucket - if not exist
-# upload content
-# set public policy - if not set
-# activate static website hosting on bucket - define index.html
-
